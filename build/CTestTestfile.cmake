8eb9cfb8d849d3538be1d89f7ecb2ac7fe988793
Dylan Edmundson <dylanedmundson@yahoo.com>
2021-04-13 20:18:29 +0100

    build folder removed

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dylanedmundson63/vsCode/txt_file_compressor
-# Build directory: /home/dylanedmundson63/vsCode/txt_file_compressor/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("txt_file_compressor-exe")
